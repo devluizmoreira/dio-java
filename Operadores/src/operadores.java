315a5a36d56dbf39df9f60638d087c6b205c303f
DevLuizMoreira <luizstringmoreira@gmail.com>
2024-05-27 19:12:12 -0300

    Manipulando Operadores

--- Operadores/src/operadores.java
+++ Operadores/src/operadores.java
@@ -1,5 +1,5 @@
 public class operadores {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         
         //Operador de atribuição é representado por =
         String nome = "Luiz";
@@ -7,7 +7,14 @@ public class operadores {
         double peso = 130.5;
         char sexo = 'M';
         boolean doadorOrgao = false;
-        Date dataNascimento = new Date();
+    
+        //Operadores Aritiméticos +, -, *, /, %
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20*7;
+        int divisao = 15/3;
+        int modulo = 18%3;
+        double resultado = (10*7) + (20/4);
 
 
     }
