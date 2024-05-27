1543dc82e477d0c9acadad4eb8c70fd2fd26ca69
DevLuizMoreira <luizstringmoreira@gmail.com>
2024-05-27 19:10:09 -0300

    Manipulando Operadores

--- Operadores/src/operadores.java
+++ Operadores/src/operadores.java
@@ -1,5 +1,14 @@
 public class operadores {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //Operador de atribuição é representado por =
+        String nome = "Luiz";
+        int idade = 30;
+        double peso = 130.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        Date dataNascimento = new Date();
+
+
     }
 }
