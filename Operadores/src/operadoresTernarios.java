a3ae3f6db6a38dc09d104424b2850d7ef400346c
DevLuizMoreira <luizstringmoreira@gmail.com>
2024-05-31 06:05:06 -0300

    invertendo valores booleanos

---
+++ Operadores/src/operadoresTernarios.java
@@ -0,0 +1,39 @@
+public class operadoresTernarios {
+    public static void main(String[] args) {
+        
+        //<Expressão condicional> ? <Caso a condição seja "true"> :
+        //Caso a condição seja "false"
+
+        int a, b;
+
+        a = 5;
+        b = 6;
+
+        /*String  resultado = "";
+        if(a==b)
+            resultado = "verdadeiro";
+        else
+            resultado = "falso";
+
+                System.out.println(resultado);*/
+
+
+        //Podemos abreviar com o operador ternário
+
+        /*resultado = a==b ? "verdadeiro" : "falso";
+                System.out.println(resqultado);
+
+        a = 6;
+
+        resultado = a==b ? "verdadeiro" : "falso";
+                System.out.println(resultado)*/
+
+        //Essa expressão booleana não se limita a utilização
+        //de variavéis String, mas também de outros tipos
+
+        int resultado = a == b ? 1: 0;
+
+
+
+  }
+}
