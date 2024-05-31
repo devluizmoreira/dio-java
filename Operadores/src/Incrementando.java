4cece9c43e5b5ec070008dadecbfa3bfc91dd9f4
DevLuizMoreira <luizstringmoreira@gmail.com>
2024-05-31 05:39:13 -0300

    Negando variaveis inteiras e booleanas

--- Operadores/src/Incrementando.java
+++ Operadores/src/Incrementando.java
@@ -18,5 +18,34 @@ public class Incrementando {
         //Para imprimir diretamente com a incrementação
         //é possível antecipando o operador de incremento
             System.out.println(++ numero);  
+
+        //A mesma regra para decrementar, utilizando o operador -
+        numero --;
+
+        //Ou imprimindo direto na tela
+            System.out.println(-- numero);
+
+        //Valores Booleanos
+
+        boolean variavel = true;
+
+        //Para negar o valor da variavel booleana criada
+        //basta utilizar o operador !
+
+        System.out.println(!variavel);
+
+        //Quando a alteração ocorre somente no comando
+        //de imprimir, fica restrita a memória
+        //O valor da variável continua sendo o mesmo
+        //Nesse caso, "true".
+
+        System.out.println(variavel);
+
+        //Para alterar o valor da variável booleana
+        //permanentemente, usamos:
+
+        variavel = !variavel;
+            System.out.println(variavel);
+
     }
 }
